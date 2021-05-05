a72a0d7e9947b4014b23eda84706c0e13adcc37b
Sean Collier <sean.collier928@yahoo.com>
2021-05-05 14:20:54 -0700

    starting a new game finished

--- connect-four.js
+++ connect-four.js
@@ -1,34 +1,46 @@
-import Game from './game.js'
-const game = undefined;
+import Game from "./game.js";
+let game = undefined;
 
-const player1Input = document.getElementById('player-1-name')
-const player2Input = document.getElementById('player-2-name');
-const newgamebttn = document.getElementById('new-game')
+function updateUI() {
+  if (game === undefined) {
+    boardHolder.classList.add("is-invisible");
+  } else {
+    boardHolder.classList.remove("is-invisible");
+    gameName.innerHTML = game.getName();
+    // console.log(gameName.innerText.value);
+  }
+}
 
-window.addEventListener('DOMContentLoaded', () => {
+const gameName = document.getElementById("game-name");
+const boardHolder = document.getElementById("board-holder");
+const player1Input = document.getElementById("player-1-name");
+const player2Input = document.getElementById("player-2-name");
+const newgamebttn = document.getElementById("new-game");
 
+window.addEventListener("DOMContentLoaded", () => {
+  player1Input.addEventListener("keyup", (event) => {
+    if (player1Input.value !== "" && player2Input.value !== "") {
+      newgamebttn.disabled = false;
+    } else {
+      newgamebttn.disabled = true;
+    }
+  });
 
-    player1Input.addEventListener('keyup', event => {
+  player2Input.addEventListener("keyup", (event) => {
+    if (player1Input.value !== "" && player2Input.value !== "") {
+      newgamebttn.disabled = false;
+    } else {
+      newgamebttn.disabled = true;
+    }
+  });
 
-        if (player1Input.value !== '' && player2Input.value !== '') {
-            newgamebttn.disabled = false
-        } else {
-            newgamebttn.disabled = true
-        }
-    })
+  newgamebttn.addEventListener("click", (event) => {
+    game = new Game(player1Input.value, player2Input.value);
+    player1Input.value = "";
+    player2Input.value = "";
+    newgamebttn.disabled = true;
+    updateUI();
+  });
+});
 
-    player2Input.addEventListener('keyup', event => {
-
-        if (player1Input.value !== '' && player2Input.value !== '') {
-            newgamebttn.disabled = false
-        } else {
-            newgamebttn.disabled = true
-        }
-    })
-
-    newgamebttn.addEventListener('click', event => {
-
-    })
-
-
-})
+// export default connect-four.js
