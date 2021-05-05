2326ab0c62b060597c9e299657e44b0f97d62368
Antonio Davila-Olivares <ajd57@nau.edu>
2021-05-05 13:40:08 -0700

    working on connect4 listeners

--- connect-four.js
+++ connect-four.js
@@ -1,25 +1,34 @@
+import Game from './game.js'
+const game = undefined;
+
+const player1Input = document.getElementById('player-1-name')
+const player2Input = document.getElementById('player-2-name');
+const newgamebttn = document.getElementById('new-game')
 
 window.addEventListener('DOMContentLoaded', () => {
 
-    import { Game } from './game.js'
-    const game = undefined;
-
-    const player1Input = document.getElementById('player-1-name')
-    const player2Input = document.getElementById('player-2-name');
 
     player1Input.addEventListener('keyup', event => {
-        const newgamebttn = getElementById('new-game')
 
         if (player1Input.value !== '' && player2Input.value !== '') {
-            newgamebttn.disabled = true
+            newgamebttn.disabled = false
         } else {
-            return 
+            newgamebttn.disabled = true
         }
-
     })
 
+    player2Input.addEventListener('keyup', event => {
 
+        if (player1Input.value !== '' && player2Input.value !== '') {
+            newgamebttn.disabled = false
+        } else {
+            newgamebttn.disabled = true
+        }
+    })
 
+    newgamebttn.addEventListener('click', event => {
+
+    })
 
 
 })
