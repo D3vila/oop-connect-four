c877fae9b825484dd304d743f3bfbf4a472b499c
Antonio Davila-Olivares <ajd57@nau.edu>
2021-05-05 11:19:39 -0700

    created game.js and working on connect4.js

--- connect-four.js
+++ connect-four.js
@@ -1,1 +1,25 @@
-console.log('HELLO')
+
+window.addEventListener('DOMContentLoaded', () => {
+
+    import { Game } from './game.js'
+    const game = undefined;
+
+    const player1Input = document.getElementById('player-1-name')
+    const player2Input = document.getElementById('player-2-name');
+
+    player1Input.addEventListener('keyup', event => {
+        const newgamebttn = getElementById('new-game')
+
+        if (player1Input.value !== '' && player2Input.value !== '') {
+            newgamebttn.classList.remove('disabled')
+        } else {
+            return
+        }
+
+    })
+
+
+
+
+
+})
